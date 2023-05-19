81a863c504c5b10f101488834f48d32de4ec0237
Inesh-Reddy <ineshreddy007@gmail.com>
2023-05-19 13:28:42 +0530

    requesting for a new account creation through aft-module

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "controltowerchild04+sandboxtest1@gmail.com"
-    AccountName               = "sandbox-afttest1"
+    AccountEmail              = "controltowerchild04+sandboxtestone@gmail.com"
+    AccountName               = "sandbox-afttestone"
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "controltowerchild04+sandboxtest1@gmail.com"
+    SSOUserEmail              = "controltowerchild04+sandboxtestone@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
